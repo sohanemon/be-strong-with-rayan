0f69ee560be67aed19cb52434c40423461b51135
sohanemon <m.sohanemon@gmail.com>
2022-11-08 16:39:29 +0600

    experience done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,17 @@
+import Experiences from "../components/experiences";
 import Hero from "../components/hero";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Experiences />
     </>
   );
 };
