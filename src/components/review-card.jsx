bcb5c149329caa9bcb4521a517f2def1523008a2
sohanemon <m.sohanemon@gmail.com>
2022-11-10 17:32:53 +0600

    modal added

--- src/components/review-card.jsx
+++ src/components/review-card.jsx
@@ -2,7 +2,15 @@ import { BsStarFill } from "react-icons/bs";
 import Rating from "react-rating";
 import { FiEdit, FiSave } from "react-icons/fi";
 import { useRef, useState } from "react";
-import { Input } from "@material-tailwind/react";
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogHeader,
+  Input,
+  Textarea,
+} from "@material-tailwind/react";
 import axios from "axios";
 import toast from "react-hot-toast";
 export default function ReviewCard({
@@ -13,18 +21,20 @@ export default function ReviewCard({
   rating,
   message,
 }) {
-  const [editMode, setEditMode] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
   const inputRef = useRef(null);
   const [messageUpdate, setMessageUpdate] = useState(message);
   const handleSave = () => {
+    handleOpen();
     axios
       .patch(`${process.env.REACT_APP_server}/reviews/${_id}`, {
-        message: inputRef.current.querySelector("input").value,
+        message: inputRef.current.querySelector("textarea").value,
       })
       .then((res) => {
         toast.success("Review updated");
-        setMessageUpdate(inputRef.current.querySelector("input").value);
-        setEditMode(false);
+        setMessageUpdate(inputRef.current.querySelector("textarea").value);
       })
       .catch((err) => console.log(err));
   };
@@ -44,31 +54,50 @@ export default function ReviewCard({
             fullSymbol={<BsStarFill className='text-red-400' />}
             emptySymbol={<BsStarFill className='text-gray-400' />}
           />
-          {edit &&
-            (!editMode ? (
+          {edit && (
+            <>
+              {" "}
               <FiEdit
-                onClick={() => setEditMode(true)}
+                onClick={() => handleOpen()}
                 className='text-xl cursor-pointer text-red-500 '
               />
-            ) : (
-              <FiSave
-                onClick={handleSave}
-                className='text-xl cursor-pointer text-red-500 '
-              />
-            ))}
+              <Dialog
+                open={open}
+                handler={handleOpen}
+                animate={{
+                  mount: { scale: 1, y: 0 },
+                  unmount: { scale: 0.9, y: -100 },
+                }}
+              >
+                <DialogHeader>Edit</DialogHeader>
+                <DialogBody>
+                  <Textarea
+                    ref={inputRef}
+                    defaultValue={messageUpdate}
+                    color='red'
+                    label='Edit review'
+                  />
+                </DialogBody>
+                <DialogFooter>
+                  <Button
+                    variant='text'
+                    color='red'
+                    onClick={handleOpen}
+                    className='mr-1'
+                  >
+                    <span>Cancel</span>
+                  </Button>
+                  <Button variant='gradient' color='red' onClick={handleSave}>
+                    <span>Confirm</span>
+                  </Button>
+                </DialogFooter>
+              </Dialog>
+            </>
+          )}
         </div>
       </div>
-      {!editMode ? (
-        <p className='ml-10 text-gray-700'>{messageUpdate}</p>
-      ) : (
-        <Input
-          ref={inputRef}
-          variant='static'
-          color='pink'
-          className=''
-          defaultValue={messageUpdate}
-        />
-      )}
+
+      <p className='ml-10 text-gray-700'>{messageUpdate}</p>
     </section>
   );
 }
